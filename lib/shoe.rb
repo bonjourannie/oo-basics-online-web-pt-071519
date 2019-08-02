22d98f6fecbaf0276a0a99d5a6661256132d4417
Annie Truex <artruex@gmail.com>
2019-08-02 23:50:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,8 @@
 # Make your shoe class here!
 class Shoe 
   
-  attr_accessor :brand, 
-  
+  attr_accessor :brand, :color, :size,
+  attr_reader
   def initialize(brand)
     @brand = brand
   end
